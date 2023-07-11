a383f0d47aaa91921a19e45358726131077d2595
Osvaldo Ortega <48293249+osortega@users.noreply.github.com>
2023-07-11 11:10:23 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  console.log(`${new Date()} ${req.method} ${req.path}`);
+  res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
